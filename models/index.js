9beeb2e4e563ab961aeb3df53542d7f5e3ed470b
Josh Carter <joshcarter8400@gmail.com>
2020-11-11 20:39:04 -0500

    added all models and associations

---
+++ models/index.js
@@ -0,0 +1,60 @@
+const User = require("./user");
+const Post = require("./post");
+const Comment = require("./comment");
+const Likes = require("./likes");
+
+// Associations
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.belongsToMany(Post, {
+  through: Likes,
+  as: "liked_posts",
+  foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+  through: Likes,
+  as: "liked_posts",
+  foreignKey: "post_id",
+});
+
+Likes.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Likes.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Likes, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Likes, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Likes, Comment };
---
+++ models/user.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+const { beforeCreate, beforeUpdate } = require("./likes");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      //set up beforecreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 8);
+        return newUserData;
+      },
+      //set up beforecreate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          8
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
