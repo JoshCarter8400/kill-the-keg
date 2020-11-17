4536b270c04b7dd9df9555206a3a2ea8d83c388e
Chris Southwick <csouthwick@live.com>
2020-11-16 19:46:58 -0500

    add notifications view and controller

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 const dashboardRoutes = require("./dashboard-routes");
+const notificationsRoutes = require("./notifications-routes");
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
+router.use("/notifications", notificationsRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
